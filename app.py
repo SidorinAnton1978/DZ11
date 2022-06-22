a1475e20e202a93ced59766a093aea1bb0b7a931
SidorinAnton1978 <ASidorin1978@gmail.com>
2022-06-23 03:48:58 +0700

    delete file classes.py, modify: utils.py, app.py, card.html, list.html, search.html, skill.html. add file Megamind.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from utils import *
 
 app = Flask(__name__)
@@ -7,8 +7,29 @@ app = Flask(__name__)
 @app.route("/")
 def page_main():
     """ Главная страница"""
-    all_candidates: str = Candidate.format_candidate()
-    return all_candidates
+    candidates = load_candidates()
+    return render_template('list.html', candidates=candidates)
+
+
+@app.route("/candidate/<int:id>")
+def page_candidate(id):
+    """Страница кандидата"""
+    candidate = candidates_id(id)
+    return render_template('card.html', candidate=candidate)
+
+
+@app.route("/search/<candidate_name>")
+def page_candidate_search(candidate_name):
+    """Страница поиска кандидатов по имени"""
+    candidates = candidates_name(candidate_name)
+    return render_template('search.html', candidates=candidates)
+
+
+@app.route("/skill/<skill>")
+def page_skills_candidate(skill):
+    """Страница поиска кандидатов по имени"""
+    candidates = candidates_skills(skill)
+    return render_template('skill.html', skill=skill, candidates=candidates)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,61 +1,36 @@
 import json
-from classes import Candidate
 
 
-def load_candidates(filename, encoding='utf-8'):
+def load_candidates(encoding='utf-8') ->list[dict]:
     """ Чтение данных из файла"""
-    with open(filename, 'r', encoding=encoding) as f:
-        raw_dict = json.load(f)
-        candidates = []
-        for candidate in raw_dict:
-            candidates.append(Candidate(candidate["id"], candidate["name"], candidate["picture"],candidate["position"],
-                                         candidate["skills"].split(", ")))
-            return candidates
+    with open('candidates.json', 'r', encoding=encoding) as f:
+        candidates = json.load(f)
+        return candidates
 
 
-
-candidates = load_candidates('candidates.json')
-print(candidates)
-
-
-
-# def format_candidate(candidates: list[dict]) -> str:
-#     """ Преобразует список кандидатов"""
-#     result = '<pre>'
-#     for candidate in candidates:
-#         result += f"""
-#             {candidate["name"]}\n
-#             {candidate["position"]}\n
-#             {candidate["skills"]}\n\n
-#         """
-#     result += '</pre>'
-#     return result
+def candidates_id(candidate_id: int) -> dict:
+    """ Поиск кандидатов по id"""
+    candidates = load_candidates()
+    for candidate in candidates:
+        if candidate['id'] == candidate_id:
+            return candidate
 
 
-# def candidates_id(id: int) -> dict:
-#     """ Поиск кандидатов по id"""
-#     candidates = load_candidates()
-#     for candidate in candidates:
-#         if candidate['id'] == id:
-#             return candidate
-#
-#
-# def candidates_skills(skill: str) -> list[dict]:
-#     """ Поиск по навыкам кандидата"""
-#     candidates = load_candidates()
-#     result = []
-#     for candidate in candidates:
-#         if skill in candidate['skills'].lower().split(', '):
-#             result.append(candidate)
-#     return result
-#
-#
-# def candidates_picture(id: int) -> str:
-#     """ Поиск картинки(фото) кандидата"""
-#     candidates = load_candidates()
-#     for candidate in candidates:
-#         if candidate['id'] == id:
-#             picture = f'<img src="{candidate["picture"]}">'
-#             return picture
+def candidates_name(name: str) -> list[dict]:
+    """ Поиск по имени кандидата"""
+    candidates = load_candidates()
+    result = []
+    for candidate in candidates:
+        if candidate["name"] == name:
+            result.append(candidate)
+    return result
 
 
+def candidates_skills(skill: str) -> list[dict]:
+    """ Поиск по навыкам кандидата"""
+    candidates = load_candidates()
+    result = []
+    for candidate in candidates:
+        if skill in candidate['skills'].lower().split(', '):
+            result.append(candidate)
+    return result
